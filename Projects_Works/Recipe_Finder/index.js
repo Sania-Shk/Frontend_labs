0144d60151a318141c23cbe819250a109b6e6066
Sania-Shk <saniashaikhhh@example.com>
2026-07-29 17:26:06 +0530

    feat : Home Page

---
+++ Projects_Works/Recipe_Finder/index.js
@@ -0,0 +1,14 @@
+// let recipe = document.querySelector("#recipe");
+// let submit = document.querySelector("#submit");
+
+// submit.addEventListener("click", mainMachine);
+
+// async function mainMachine() {
+//   let data = recipe.value.toLowerCase();
+//   let apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${data}`;
+
+//   let fetchData = await fetch(apiUrl);
+//   const response = await fetchData.json();
+
+//   return console.log(response);
+// }
