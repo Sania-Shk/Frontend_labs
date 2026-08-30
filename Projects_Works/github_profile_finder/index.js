ea3e03f5eea6fe22053d3843e0c1c27517d8119f
Sania-Shk <saniashaikhhh@example.com>
2026-08-30 20:10:23 +0530

    feat : automate user repo box

--- Projects_Works/github_profile_finder/index.js
+++ Projects_Works/github_profile_finder/index.js
@@ -7,6 +7,10 @@ let userBio = document.querySelector("#userBio");
 let followernum = document.querySelector(".followernum");
 let followingnum = document.querySelector(".followingnum");
 
+let joinDate = document.querySelector("#joined");
+
+let repoList = document.querySelector(".repoList");
+
 userbutton.addEventListener("click", getuserporfile);
 
 async function getuserporfile() {
@@ -21,15 +25,62 @@ async function getuserporfile() {
   const repo_response = await repodata.json();
 
   userDetail(respone);
-  return console.log(repo_response);
+  userRepo(repo_response);
 }
 
 function userDetail(info) {
   let userInfo = info;
 
-  // userName.textContent = userInfo.name;
+  userName.textContent = userInfo.name;
   userId.textContent = userInfo.login;
-  userBio.textContent = userInfo.bio;
+
+  if (!userInfo.bio) {
+    userBio.textContent = "This profile has no bio";
+  } else {
+    userBio.textContent = userInfo.bio;
+  }
   followernum.textContent = userInfo.followers;
   followingnum.textContent = userInfo.following;
+
+  const optionsIn = { day: "numeric", month: "long", year: "numeric" }; //  converting date
+  joinDate.textContent = new Date(userInfo.created_at).toLocaleDateString(
+    "en-In",
+    optionsIn,
+  );
+}
+
+function userRepo(RepoInfo) {
+  let repos = RepoInfo;
+
+  repos.forEach((element) => {
+    repobox = document.createElement("div");
+    repobox.className = "box";
+
+    repohead = document.createElement("div");
+    repohead.className = "repohead";
+
+    repofile = document.createElement("img");
+    repofile.className = "filelogo";
+    repofile.src = "assets/images/fileLogo.svg";
+
+    repotitle = document.createElement("h2");
+    repotitle.className = "repotitle";
+    repotitle.textContent = element.name;
+
+    reposdecription = document.createElement("div");
+    reposdecription.className = "description";
+    reposdecription.textContent = element.description;
+
+    reposlanguage = document.createElement("div");
+    reposlanguage.className = "language";
+    reposlanguage.textContent = element.language;
+
+    repohead.appendChild(repofile);
+    repohead.appendChild(repotitle);
+    repobox.appendChild(repohead);
+    repobox.appendChild(reposdecription);
+    repobox.appendChild(reposlanguage);
+
+    repoList.appendChild(repobox);
+  });
 }
